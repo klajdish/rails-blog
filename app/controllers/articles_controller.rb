89011100c893bbe9b934453699814ebd8c0f5245
klajdish <shehaj.klajdi@outlook.com>
2023-12-09 16:38:50 +0100

    create and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,41 @@
 class ArticlesController < ApplicationController
   def show
+    # render plain: params;
     # abort params.inspect
-    @article = Article.find(1)
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @test = '123'
+
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id]);
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    result = @article.update(params.require(:article).permit(:title, :description));
+
+    if result
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
   # get "article/:id" => "articles#show"
 end
